e341308b423dedece78fd947daca1a3e0dd6383a
artem.yankovets <Artem.Yankovets@b-next.com>
2017-10-24 13:17:56 +0300

    add several methods for handling REST requests

--- src/main/java/com/wishlist/serverside/web/WishListController.java
+++ src/main/java/com/wishlist/serverside/web/WishListController.java
@@ -2,12 +2,11 @@ package com.wishlist.serverside.web;
 
 
 import com.wishlist.serverside.domain.User;
+import com.wishlist.serverside.domain.Wish;
 import com.wishlist.serverside.persistance.UserRepository;
 import com.wishlist.serverside.persistance.WishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,14 +17,36 @@ public class WishListController {
     @Autowired
     private UserRepository userRepository;
 
-    public WishListController(UserRepository userRepository) {
+    @Autowired
+    private WishRepository wishRepository;
+
+    public WishListController(UserRepository userRepository, WishRepository wishRepository) {
         this.userRepository = userRepository;
+        this.wishRepository = wishRepository;
     }
 
     /* users */
-    @GetMapping(value = "/users/all")
+    @RequestMapping (value = "/users/all", method = RequestMethod.GET)
     public List<User> getAllUsers() {
         return this.userRepository.findAll();
     }
+
+    @RequestMapping (value = "/users/new", method = RequestMethod.PUT)
+    public void insert(@RequestBody User user) {
+        this.userRepository.insert(user);
+    }
+
+    /* wishes */
+    @RequestMapping (value = "/wishes/all", method = RequestMethod.GET)
+    public List<Wish> getAllWishes() {
+        return this.wishRepository.findAll();
+    }
+
+    @RequestMapping (value = "/wishes/remove/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        this.wishRepository.delete(id);
+    }
+
+
 
 }
