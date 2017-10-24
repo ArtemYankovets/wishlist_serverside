bf6143a12636156df08ac2caeb613f8ecc4cf332
artem.yankovets <Artem.Yankovets@b-next.com>
2017-10-24 13:14:07 +0300

    add fingById and findByUserUsageRreferences methods

--- src/main/java/com/wishlist/serverside/persistance/WishRepository.java
+++ src/main/java/com/wishlist/serverside/persistance/WishRepository.java
@@ -1,9 +1,15 @@
 package com.wishlist.serverside.persistance;
 
+import com.wishlist.serverside.domain.User;
 import com.wishlist.serverside.domain.Wish;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WishRepository extends MongoRepository<Wish, String>{
+    Wish findById(String id);
+
+    List<User> findByUserUsageRreferences(String userId);
 }
