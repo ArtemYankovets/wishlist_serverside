05093c09cb30ed92efc99eca54bb3cf0ef18132a
artem.yankovets <Artem.Yankovets@b-next.com>
2017-10-25 14:29:36 +0300

    changed service according to the adding new domain and changing domain model

--- src/main/java/com/wishlist/serverside/service/WishListService.java
+++ src/main/java/com/wishlist/serverside/service/WishListService.java
@@ -3,7 +3,9 @@ package com.wishlist.serverside.service;
 import com.wishlist.serverside.domain.Address;
 import com.wishlist.serverside.domain.User;
 import com.wishlist.serverside.domain.Wish;
+import com.wishlist.serverside.domain.WishList;
 import com.wishlist.serverside.persistance.UserRepository;
+import com.wishlist.serverside.persistance.WishListRepository;
 import com.wishlist.serverside.persistance.WishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,57 +24,29 @@ public class WishListService implements CommandLineRunner {
     @Autowired
     private WishRepository wishRepository;
 
+    @Autowired
+    private WishListRepository wishListRepository;
+
     @Override
     public void run(String... strings) throws Exception {
 
         initUsersCollection();
         initWishesCollection();
+        initWishListsCollection();
 
         User user = userRepository.findById("userAlanPupkin");
-        Wish wish = wishRepository.findById("wish001");
+        Wish wish001 = wishRepository.findById("wish001");
+        WishList wishList001 = wishListRepository.findById("wishList001");
 
-        user.getListOfWishesId().add(wish.getId());
-        wish.getUserUsageRreferences().add(user.getId());
+
+
+        user.getWishListIds().add(wishList001.getId());
+        wishList001.setOwner(user.getId());
+        wish001.getUserUsageId().add(user.getId());
 
         userRepository.save(user);
-        wishRepository.save(wish);
-    }
-
-    private void initWishesCollection() {
-        Wish wishOne = new Wish(
-                "wish001",
-                "Finish wishlist app",
-                false,
-                new ArrayList<>()
-        );
-
-        Wish wishTwo = new Wish(
-                "wish002",
-                "Launch wishlist app",
-                false,
-                new ArrayList<>()
-        );
-
-        Wish wishThree = new Wish(
-                "wish003",
-                "Read book",
-                false,
-                new ArrayList<>()
-        );
-
-        Wish wishFour = new Wish(
-                "wish004",
-                "Go to the gim",
-                false,
-                new ArrayList<>()
-        );
-
-        // drop all wishes
-        wishRepository.deleteAll();
-
-        // add all wishes to the database
-        List<Wish> wishes = Arrays.asList(wishOne, wishTwo, wishThree, wishFour);
-        wishRepository.save(wishes);
+        wishListRepository.save(wishList001);
+        wishRepository.save(wish001);
     }
 
     private void initUsersCollection() {
@@ -104,5 +78,69 @@ public class WishListService implements CommandLineRunner {
         // all users to the database
         List<User> users = Arrays.asList(alan, maria);
         userRepository.save(users);
+    }
+
+    private void initWishesCollection() {
+        Wish wishOne = new Wish(
+                "wish001",
+                "Finish wishlist app",
+                false,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+        Wish wishTwo = new Wish(
+                "wish002",
+                "Launch wishlist app",
+                false,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+        Wish wishThree = new Wish(
+                "wish003",
+                "Read book",
+                false,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+        Wish wishFour = new Wish(
+                "wish004",
+                "Go to the gim",
+                false,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+        // drop all wishes
+        wishRepository.deleteAll();
+
+        // add all wishes to the database
+        List<Wish> wishes = Arrays.asList(wishOne, wishTwo, wishThree, wishFour);
+        wishRepository.save(wishes);
+    }
+
+    private void initWishListsCollection() {
+        WishList wishListOne = new WishList(
+                "wishlist001",
+                "Traveling",
+                "",
+                new ArrayList<>()
+        );
+
+        WishList wishListTwo = new WishList(
+                "wishlist002",
+                "Self improvement",
+                "",
+                new ArrayList<>()
+        );
+
+        // drop all wishes
+        wishListRepository.deleteAll();
+
+        // add all wishes to the database
+        List<WishList> wishLists = Arrays.asList(wishListOne, wishListTwo);
+        wishListRepository.save(wishLists);
     }
 }
