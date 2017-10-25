ee56ff4f2a9bf0582c9037226bdb956523fff79f
artem.yankovets <Artem.Yankovets@b-next.com>
2017-10-25 18:47:54 +0300

    Create class with constants

---
+++ src/main/java/com/wishlist/serverside/web/ConstantMessages.java
@@ -0,0 +1,6 @@
+package com.wishlist.serverside.web;
+
+public class ConstantMessages {
+    public static final String RESOURCE_IS_ALREADY_EXIST = "{\"message\":\"Resource is already exist\"}";
+    public static final String RESOURCE_DOES_NOT_EXIST = "{\"message\":\"Resource doesn't exist\"}";
+}
